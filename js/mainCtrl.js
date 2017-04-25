dc599237f187031da3c6a8fb0783b593b27b1ab8
loren pabst <loren pabst>
2017-04-25 14:27:48 -0600

    add and delete buttons are working and styled

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -4,9 +4,14 @@ angular.module('myApp')
 
 $scope.quotes = mainService.getQuotes();
 
+$scope.addQuote = mainService.addQuote;
+$scope.deleteQuote = mainService.deleteQuote;
 
+var show = true;
 
-
+$scope.showInput = function(){
+    $scope.show = !$scope.show;
+};
 
 
 
